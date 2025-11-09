a481f287ed412d370626cf5ecd618de741981a97
TylerMcGiboney <tylerz4567@gmail.com>
2025-11-08 19:20:49 -0700

    Created Database Service and added MySql to the project.

--- AppointmentScheduler/Services/DatabaseService.cs
+++ AppointmentScheduler/Services/DatabaseService.cs
@@ -3,10 +3,31 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Configuration;
+using MySql.Data.MySqlClient;
+
 
 namespace AppointmentScheduler.Services
 {
-    class DatabaseService
+    public class DatabaseService
     {
+        public static MySqlConnection GetConnection()
+        {
+            var connectionStringSettings = ConfigurationManager.ConnectionStrings["localdb"];
+            
+            if(connectionStringSettings == null)
+            {
+                throw new InvalidOperationException("The connection string to the database can not be found.");
+            }
+
+            string connectionString = connectionStringSettings.ConnectionString;
+
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                throw new InvalidOperationException("The connection string: 'localdb' is empty.");
+            }
+            return new MySqlConnection(connectionString);
+        }
+
     }
 }
