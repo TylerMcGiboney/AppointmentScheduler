042c295718c2cc1446e47b1103107c528c64dcbe
TylerMcGiboney <tylerz4567@gmail.com>
2025-11-08 21:32:41 -0700

    Added exception handling and database queries for country

--- AppointmentScheduler/Repositories/CountryRepository.cs
+++ AppointmentScheduler/Repositories/CountryRepository.cs
@@ -3,10 +3,143 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AppointmentScheduler.Models;
+using AppointmentScheduler.Services;
+using MySql.Data.MySqlClient;
+using ZstdSharp;
 
 namespace AppointmentScheduler.Repositories
 {
-    class CountryRepository
+    public class CountryRepository
     {
+        public List<Country> GetAllCountries()
+        {
+            List<Country> countries = new List<Country>();
+            try
+            {
+                using (MySqlConnection conn = DatabaseService.GetConnection())
+                {
+                    conn.Open();
+                    string query = "SELECT * FROM country";
+
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+
+                    using (MySqlDataReader rdr = cmd.ExecuteReader())
+                    {
+                        while (rdr.Read()){
+                            Country c = new Country
+                            {
+                                CountryId = Convert.ToInt32(rdr["countryId"]),
+                                CountryName = Convert.ToString(rdr["country"]),
+                                CreateDate = Convert.ToDateTime(rdr["createDate"]),
+                                CreatedBy = Convert.ToString(rdr["createdBy"]),
+                                LastUpdate = Convert.ToDateTime(rdr["lastUpdate"]),
+                                LastUpdateBy = Convert.ToString(rdr["lastUpdateBy"])
+                            };
+                            countries.Add(c);
+                        }
+                    }
+                }
+                return countries;
+            }
+            catch(MySqlException ex)
+            {
+                throw new ApplicationException(ExceptionHandler.GetMessage(ex, "Get countries"), ex);
+            }   
+        }
+
+        public int AddCountry(Country country)
+        {
+            try {
+                using(MySqlConnection conn = DatabaseService.GetConnection())
+                {
+                    conn.Open();
+
+                    string query = "INSERT INTO country (country,createDate,createdBy,lastUpdate,lastUpdateBy)" +
+                                    "VALUES (@country,@createDate,@createdBy,@lastUpdate,@lastUpdateBy)";
+                    MySqlCommand cmd = new MySqlCommand(query, conn);
+                    cmd.Parameters.AddWithValue("@country", country.CountryName);
+                    cmd.Parameters.AddWithValue("@createDate", DateTime.UtcNow);
+                    cmd.Parameters.AddWithValue("@createdBy", App.CurrentUser.UserName);
+                    cmd.Parameters.AddWithValue("@lastUpdate", DateTime.UtcNow);
+                    cmd.Parameters.AddWithValue("@lastUpdateBy", App.CurrentUser.UserName);
+
+                    int rowsAffected = cmd.ExecuteNonQuery();
+
+                    if(rowsAffected == 0)
+                    {
+                        throw new Exception("Failed to add country.");
+                    }
+
+                    long newId = cmd.LastInsertedId;
+                    return (int)newId;
+                }
+            }
+            catch(MySqlException ex)
+            {
+                throw new ApplicationException(ExceptionHandler.GetMessage(ex, "Add country"), ex);
+            }
+        }
+
+        public Country GetByName(string name)
+        {
+            try
+            {
+                using (MySqlConnection conn = DatabaseService.GetConnection())
+                {
+                    conn.Open();
+
+                    string query = "SELECT * FROM country WHERE country = @country LIMIT 1";
+
+                    using (MySqlCommand cmd = new MySqlCommand(query, conn))
+                    {
+                        cmd.Parameters.AddWithValue("@country", name);
+
+                        using (MySqlDataReader rdr = cmd.ExecuteReader())
+                        {
+                            if (rdr.Read())
+                            {
+                                return new Country
+                                {
+                                    CountryId = Convert.ToInt32(rdr["countryId"]),
+                                    CountryName = Convert.ToString(rdr["country"]),
+                                    CreateDate = Convert.ToDateTime(rdr["createDate"]),
+                                    CreatedBy = Convert.ToString(rdr["createdBy"]),
+                                    LastUpdate = Convert.ToDateTime(rdr["lastUpdate"]),
+                                    LastUpdateBy = Convert.ToString(rdr["lastUpdateBy"])
+                                };
+                            }
+                        }
+                    }
+                }
+                return null;
+            }
+            catch (MySqlException ex)
+            {
+                throw new ApplicationException(ExceptionHandler.GetMessage(ex, "Look up country by name"), ex);
+            }
+        }
+
+        public int GetOrCreateCountry(String name)
+        {
+            string normalizedName = name.Trim();
+            Country existingCountry = GetByName(normalizedName);
+
+            if(existingCountry != null)
+            {
+                return existingCountry.CountryId;
+            }
+            else
+            {
+                Country newCountry = new Country
+                {
+                    CountryName = normalizedName
+                };
+
+                int newId = AddCountry(newCountry);
+                return newId;
+                
+            }
+        }
     }
 }
---
+++ AppointmentScheduler/Services/ExceptionHandler.cs
@@ -0,0 +1,34 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppointmentScheduler.Services
+{
+    public static class ExceptionHandler
+    {
+        public static string GetMessage(MySqlException ex, string context)
+        {
+            string message;
+
+            if (ex.Number == 1062)
+                message = $"Duplicate entry error occurred while {context}. Please ensure all unique fields are unique.";
+            else if (ex.Number == 1451)
+                message = $"Cannot delete or update because of related records while {context}. Please remove related records first.";
+            else if (ex.Number == 1452)
+                message = $"Foreign key constraint fails while {context}. Please ensure all referenced records exist.";
+            else if (ex.Number == 1048)
+                message = $"A required field is missing (Error {ex.Number}) while {context}: {ex.Message}";
+            else
+                message = $"A database error occurred (Error {ex.Number}) while {context}: {ex.Message}";
+
+            if (!string.IsNullOrEmpty(context))
+                message = context = ": " + message;
+
+            return message;
+
+        }
+    }
+}
