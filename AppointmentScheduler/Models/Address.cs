91887d6820a1571dda765059067a728d3d79d8ba
TylerMcGiboney <tylerz4567@gmail.com>
2025-11-08 18:58:46 -0700

    Created Address model

--- AppointmentScheduler/Models/Address.cs
+++ AppointmentScheduler/Models/Address.cs
@@ -6,7 +6,17 @@ using System.Threading.Tasks;
 
 namespace AppointmentScheduler.Models
 {
-    class Address
+    public class Address
     {
+        int AddressId { get; set; }
+        string Address1 { get; set; }
+        string Address2 { get;set; }
+        int CityId { get;set; }
+        string Zip { get; set; }
+        string PhoneNumber { get;set; }
+        DateTime CreateDate { get; set; }
+        string CreatedBy { get; set; }
+        DateTime LastUpdate { get; set; }
+        string LastUpdateBy { get;set; }
     }
 }
