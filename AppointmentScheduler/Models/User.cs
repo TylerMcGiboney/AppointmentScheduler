907776effada00fa71608c9e109bd46df8771a51
TylerMcGiboney <tylerz4567@gmail.com>
2025-11-08 19:04:42 -0700

    Created User model

--- AppointmentScheduler/Models/User.cs
+++ AppointmentScheduler/Models/User.cs
@@ -6,7 +6,16 @@ using System.Threading.Tasks;
 
 namespace AppointmentScheduler.Models
 {
-    class User
+    public class User
     {
+        int Id { get; set; }
+        string UserName { get; set; }
+        string Password { get; set; }
+        bool IsActive { get; set; }
+        DateTime CreateDate { get; set; }
+        string CreatedBy { get; set; }
+        DateTime LastUpdate { get; set; }
+        string LastUpdateBy { get; set; }
+
     }
 }
