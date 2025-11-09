a8f1de355b7a6a3b81e0826fd73afa74bbb8a99b
TylerMcGiboney <tylerz4567@gmail.com>
2025-11-08 19:00:24 -0700

    Completed Appointment model

--- AppointmentScheduler/Models/Appointment.cs
+++ AppointmentScheduler/Models/Appointment.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace AppointmentScheduler.Models
 {
-    class Appointment
+    public class Appointment
     {
+        int AppointmentId { get; set; }
+        int CustomerId { get; set; }
+        int UserId { get; set; }
+        string Title { get; set; }
+        string Description { get; set; }
+        string Location { get; set; }
+        string Contact { get; set; }
+        string Type { get; set; }
+        string Url { get; set; }
+        DateTime Start { get; set; }
+        DateTime End { get; set; }
+        DateTime CreateDate { get; set; }
+        string CreatedBy { get; set; }
+        DateTime LastUpdate { get; set; }
+        string LastUpdateBy { get; set; }
+
     }
 }
